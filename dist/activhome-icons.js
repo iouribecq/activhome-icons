96886de90b299daed607fb38cb8d7b998df5260a
Iouri Becq <iouribecq@gmail.com>
2026-01-11 20:22:06 +0100

    fix: make HACS dashboard repo compliant (dummy js + filename)

---
+++ dist/activhome-icons.js
@@ -0,0 +1,6 @@
+﻿/*
+ * Activhome Icons (HACS)
+ * This is a minimal Lovelace plugin file required by HACS.
+ * The actual purpose of this repository is to distribute SVG icons under dist/icons/.
+ */
+console.debug("[Activhome Icons] loaded");
